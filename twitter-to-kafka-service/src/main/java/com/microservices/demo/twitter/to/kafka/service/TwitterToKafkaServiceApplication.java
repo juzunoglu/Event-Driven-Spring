57ef3bb42a74238fb1d5071af5ada699f1c8923c
juzunoglu <uzunoglualihan@gmail.com>
2022-11-07 21:06:33 +0300

    twit streaming with filters

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,35 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import twitter4j.TwitterException;
+
+@SpringBootApplication
+@Slf4j
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData, StreamRunner streamRunner) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.streamRunner = streamRunner;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws TwitterException {
+        log.info("App starts...");
+        log.info(twitterToKafkaServiceConfigData.getTwitterKeywords().toString());
+        streamRunner.start();
+    }
+}
---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
@@ -0,0 +1,17 @@
+package com.microservices.demo.twitter.to.kafka.service.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Slf4j
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfigData {
+
+    private List<String> twitterKeywords;
+}
